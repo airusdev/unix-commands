5627597ea5e4da228b7335c87f4a001a1cb5be0f
airusdev <work.airus@gmail.com>
2026-07-02 18:19:14 +0800

    added when theres no file, and when file is detected

--- cat.py
+++ cat.py
@@ -1,30 +1,40 @@
 import argparse
 import sys
-import keyboard
 
 def sys_write(message: str) -> None:
     """Shortens sys.stdout.write and flushes with a newline"""
     sys.stdout.write(message + "\n")
 
-def detect_shortcut() -> None:
-    global shortcut_pressed
-    shortcut_pressed = True
+def sys_error(message: str) -> None:
+    sys.stderr.write(message + "\n")
 
-# def shortcut_listener() -> None:
-#     keyboard.add_hotkey("ctrl+m", detect_shortcut)
+def number_output_lines() -> None:
+    return None
 
-def acquire_input(message: str) -> str:
-    user_input = input(message)
-    return user_input
+def number_blank_lines() -> None:
+    return None
 
 def file_not_detected() -> None:
-    while True:
-        user_input = acquire_input("acquiring user input: ")
-        sys_write(user_input + '\n')
+    """When file is not inputted, acquire datafrom stdin instead"""
+    sys_write("file not detected") 
+    sys_write("acquiring user input: \n") 
+    
+    for line in sys.stdin:
+        user_input = line.strip()
+        sys_write("inputted: " + user_input)
 
-        if keyboard.is_pressed("ctrl+d"):
-            sys_write("successfully stopped")
-            break
+def file_detected() -> None:
+    "File/s are detected, output the data to the terminal"
+    sys_write("file/s detected" + "\n")
+    
+    for file in args.file:
+        try:
+            with open(f"./sample_files/{file}", 'r', encoding="utf-8") as f:
+                for line in f:
+                    line = line.strip()
+                    sys_write(line)
+        except FileNotFoundError:
+            sys_error(f"[ERROR DETECTED] File: {file} is not found")
 
 def setup_parser() -> None:
     """Sets up argparse and the arguments"""
@@ -39,16 +49,17 @@ def setup_parser() -> None:
 
     args = parser.parse_args()
 
-def detect_cat() -> None: # needs a better name for this
+def cat() -> None: # needs a better name for this
     if not args.file:
         file_not_detected()        
     else:
-        sys_write("files detected")
+        file_detected()
+
 
 
 def main() -> None:
     setup_parser()
-    detect_cat()    
+    cat()    
 
 
 if __name__ == "__main__":
