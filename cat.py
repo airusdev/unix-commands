3de19f2bb0487c04e3d66484e0739a9afb250323
airusdev <work.airus@gmail.com>
2026-07-04 13:26:35 +0800

    refactored the output functions

--- cat.py
+++ cat.py
@@ -9,46 +9,45 @@ def sys_write(message: str) -> None:
 def sys_error(message: str) -> None:
     sys.stderr.write(message + "\n")
 
-def file_only(f: TextIO) -> None:
-    for line in f:
-        line = line.strip()
-        sys_write(line)
-
-def number_output_lines(f: TextIO) -> None:
-    count = 1
-    for line in f:
-        line = line.strip()
-        sys_write(f"{count} {line}")
-        count += 1
-
-def number_non_blank_lines(f: TextIO) -> None:
-    count = 1
-    for line in f:
-        line = line.strip()
-        if line != "":
-            sys_write(f"{count} {line}")
-            count += 1
-        else:
-            sys_write(line)
-
 def setup_parser() -> None:
     """Sets up argparse and the arguments"""
     global parser
     global args
-
-    parser = argparse.ArgumentParser()
     
+    parser = argparse.ArgumentParser()
+
     parser.add_argument("file", help="file you wish to work with", nargs="*")
     parser.add_argument("-n", "--number_output_lines", help="number every output line", action="store_true")
     parser.add_argument("-b", "--number_non_blank_lines", help="number every non-blank lines", action="store_true")
 
     args = parser.parse_args()
+    
+def output_content(file: TextIO) -> str | None:
+    """Outputs the content based on the optional argument picked"""
+    count = 1
+    
+    for line in file:
+        line =  line.strip()
+        
+        if args.number_output_lines:
+            sys_write(f"{count} {line}")
+            count += 1
+
+        elif args.number_non_blank_lines:
+            if line == "":
+                sys_write(f"{count} {line} NON BLANK LINES DETECTED")
+                count += 1
+            else:
+                sys_write(line)
+
+        else:
+            sys_write(line)
 
 def file_not_detected() -> None:
     """When file is not inputted, acquire datafrom stdin instead"""
-    sys_write("file not detected") 
-    sys_write("acquiring user input: \n") 
-    
+    sys_write("file not detected")
+    sys_write("acquiring user input: \n")
+
     for line in sys.stdin:
         user_input = line.strip()
         sys_write("inputted: " + user_input)
@@ -56,25 +55,19 @@ def file_not_detected() -> None:
 def file_detected() -> None:
     "File/s are detected, output the data to the terminal"
     sys_write("file/s detected" + "\n")
-    
+
     for file in args.file:
         try:
             with open(f"./sample_files/{file}", 'r', encoding="utf-8") as f:
-                if args.number_output_lines:
-                    number_output_lines(f)
-                        
-                elif args.number_non_blank_lines:
-                    number_non_blank_lines(f)
-                
-                else:
-                    file_only()
-                    
+                output_content(f)
+
         except FileNotFoundError:
             sys_error(f"[ERROR DETECTED] File: {file} is not found")
 
-def cat() -> None: # needs a better name for this
+def cat() -> None:
+    """Calls the appropriate functions to output content to the terminal"""
     if not args.file:
-        file_not_detected()        
+        file_not_detected()
     else:
         file_detected()
 
@@ -82,10 +75,9 @@ def cat() -> None: # needs a better name for this
 
 def main() -> None:
     setup_parser()
-    cat()    
+    cat()
 
 
 if __name__ == "__main__":
     main()
 
-    
