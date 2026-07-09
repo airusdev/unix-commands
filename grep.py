f38c620c4c78b9f9f0d1a7b450323ec62b9e44d5
airusdev <work.airus@gmail.com>
2026-07-09 13:32:06 +0800

    add number_output_lines, count_of_matches, case_insensitive, and invert_match optional flags

--- grep.py
+++ grep.py
@@ -20,24 +20,70 @@ def setup_argparse() -> None:
     # optional arguments
     parser.add_argument("-n", "--number_output_lines", help="print line numbers alongside matches", action="store_true")
     parser.add_argument("-c", "--count_of_matches", help="print only the count of matching lines", action="store_true")
-    parser.add_argument("-i", "--case_insenstive", help="case-insensitive matching", action="store_true")
+    parser.add_argument("-i", "--case_insensitive", help="case-insensitive matching", action="store_true")
     parser.add_argument("-v", "--invert_match", help="invert match, print lines that do NOT match", action="store_true")
 
     args = parser.parse_args()
 
 def output_content(file: TextIO) -> None:
-    count = 1 
+    print(f"pattern: {args.pattern}")
+    count = 1
     
     for line in file:
         line = line.strip()
+        pattern = args.pattern
         to_print = line
-
-        if args.pattern in line:
-            if args.number_output_lines:
-                to_print = f"{count} {line}"
-                count += 1
+        
+        if args.case_insensitive:
+            pattern = args.pattern.lower()
+            line = line.lower()
+        
+        if args.number_output_lines:
+            to_print = f"{count} {line}"
+        
+        # REWRITE THIS PART
+        if args.invert_match:
+            if pattern in line:
+                continue
             
-            sys_write(to_print)
+            if args.count_of_matches:
+                count += 1
+                continue
+            else:
+                sys_write(to_print)
+                count += 1
+        else:
+            if not pattern in line:
+                continue
+            
+            if args.count_of_matches:
+                count += 1
+                continue
+            else:
+                sys_write(to_print)
+                count += 1
+
+    if args.count_of_matches:
+        sys_write(str(count - 1))
+        
+        # if pattern in line:
+        #     sys_write(to_print)
+        #     count += 1
+
+        
+        # else:
+        #     if args.pattern in line:
+        #         to_print = f"{count} {line}"
+        #         count += 1
+        
+
+        # if args.pattern in line:
+        #     if args.number_output_lines:
+        #         to_print = f"{count} {line}"
+        #         count += 1
+        #     if args.
+            
+        #     sys_write(to_print)
         
 
 def file_pattern_detected() -> None:
