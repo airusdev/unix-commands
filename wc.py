31abea00348641fa30c6e495fbff2e48c5235654
airusdev <work.airus@gmail.com>
2026-07-12 08:56:12 +0800

    make acquire from stdin functional and add the three optional arguments: lines_only, words_only, and bytes_only

--- wc.py
+++ wc.py
@@ -37,12 +37,39 @@ def setup_variables() -> None:
     bytes_ = 0
 
 def output_results() -> None:
-    sys_write(f"{lines} {words} {bytes_}") 
+    global lines
+    global words
+    global bytes_
+
+    to_output = []
+    default = f"{lines} {words} {bytes_}"
+
+    if args.lines_only:
+        to_output.append(lines)
+    if args.words_only:
+        to_output.append(words)
+    if args.bytes_only:
+        to_output.append(bytes_)
+
+    if not to_output:
+        to_output = default
+
+    to_output = " ".join(map(str, to_output))
+    sys_write(to_output)
+
+def count_line(line: str) -> int:
+    if "\n" in line:
+        return 1
+
+    return 0
 
 def count_words(line: str) -> int:
     line = line.strip().split(" ")
     word_count = len(line)
-    
+
+    if line[0] == '':
+        return 0
+
     return word_count
 
 def count_bytes(line: str) -> int:
@@ -51,16 +78,14 @@ def count_bytes(line: str) -> int:
 
     return bytes_count
 
-def count_occurrences_in_file(file: TextIO) -> None:   
+def count_occurrences_in_file(file: TextIO) -> None:
     """Acquires the total number of occurrences of lines, word, and bytes"""
     global lines
     global words
-    global bytes_ 
-    
-    for line in file:
-        line = line.strip()
+    global bytes_
 
-        lines += 1
+    for line in file:
+        lines += count_line(line)
         words += count_words(line)
         bytes_ += count_bytes(line)
 
@@ -82,13 +107,18 @@ def look_for_file_matches() -> None:
 
 def file_not_exists() -> None:
     """If file does not exist, acquire from stdin"""
+    global lines
+    global words
+    global bytes_
+
     sys_write("[NO FILE DETECTED] Acquiring from stdin . . .")
 
     for line in sys.stdin:
-        line = line.strip()
-        sys_write(line)
-        # code here
+        lines += count_line(line)
+        words += count_words(line)
+        bytes_ += count_bytes(line)
 
+        sys_write(f"{lines} {words} {bytes_}")
 
 def file_existence() -> None:
     """Validates if file from input exists"""
@@ -98,7 +128,6 @@ def file_existence() -> None:
         file_not_exists()
 
     return
-
 
 def wc() -> None:
     file_existence()
