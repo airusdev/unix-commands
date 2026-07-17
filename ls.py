85d6ad48bf05a93c8d742e76a9b42e3e6059c482
airusdev <work.airus@gmail.com>
2026-07-17 19:42:43 +0800

    simple setup

--- ls.py
+++ ls.py
@@ -1,1 +1,16 @@
 import argparse
+
+def setup_argparse() -> None:
+    parser = argparse.ArgumentParser()
+    
+    # positional argument/s
+     
+     
+    # optional argument/s
+
+
+def main() -> None:
+    setup_argparse()
+
+if __name__ == '__main__':
+    main()
